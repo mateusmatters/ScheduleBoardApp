95aa2211cf44b85236a555d72c350dd43954ad5d
mateusmatters <mateuskelly82@gmail.com>
2025-04-09 16:06:21 -0400

    add more employees to test out

--- src/class definitions/testEmployeesArray.js
+++ src/class definitions/testEmployeesArray.js
@@ -52,65 +52,88 @@ import { createTimeStamp } from "./helperFunctions.js";
 
 
 const employeeArray = new SortedArray(Employee);
-// employeeArray.add(new Employee("DONALD", "WOODS", createTimeStamp(8,30), createTimeStamp(9,30), "Cashier Asst", "FRONT-END")); //WEIRD SEG
-// employeeArray.add(new Employee("LIDA", "CARRANZA", createTimeStamp(9,30), createTimeStamp(18,0), "Cashier", "FRONT-END"));
-// employeeArray.add(new Employee("GUPTA", "SUDESH", createTimeStamp(9,30), createTimeStamp(18,0), "Cashier", "FRONT-END"));
-// employeeArray.add(new Employee("MOHAMMED", "SARA", createTimeStamp(9,30), createTimeStamp(18,0), "Cashier", "FRONT-END"));
-// employeeArray.add(new Employee("PETROGRADSKIY", "ARKADIY", createTimeStamp(9,30), createTimeStamp(18,0), "Cashier", "FRONT-END"));
-// employeeArray.add(new Employee("COLE", "DAVID", createTimeStamp(10,0), createTimeStamp(18,30), "Cashier", "FRONT-END"));
-// employeeArray.add(new Employee("ANGELITA", "TAPIA", createTimeStamp(10,0), createTimeStamp(18,30), "Cashier", "FRONT-END"));
-// employeeArray.add(new Employee("GAIL", "GRAYBILL", createTimeStamp(10,15), createTimeStamp(18,45), "Cashier", "FRONT-END"));
-// employeeArray.add(new Employee("LYDIA", "OPOKU", createTimeStamp(10,15), createTimeStamp(18,45), "Cashier", "FRONT-END"));
-// employeeArray.add(new Employee("EVELYN", "REYES", createTimeStamp(10,15), createTimeStamp(18,45), "Cashier", "FRONT-END"));
-// employeeArray.add(new Employee("HIRUTE", "TAKELE", createTimeStamp(10,15), createTimeStamp(18,45), "Cashier", "FRONT-END"));
-// employeeArray.add(new Employee("ZOILA", "FLORES", createTimeStamp(10,30), createTimeStamp(15,30), "Cashier", "FRONT-END"));
-// employeeArray.add(new Employee("KENNEDY", "AMPADU", createTimeStamp(10,30), createTimeStamp(12,30), "Cashier", "FRONT-END")); //WEIRD SEG
-// employeeArray.add(new Employee("HANIF", "MOHAMMAD", createTimeStamp(10,30), createTimeStamp(15,0), "Cashier", "FRONT-END")); 
-// employeeArray.add(new Employee("SAAD", "ANDRAOS", createTimeStamp(10,30), createTimeStamp(19,0), "Cashier Asst", "FRONT-END"));
-// employeeArray.add(new Employee("MARGARET", "NTIM", createTimeStamp(10,45), createTimeStamp(15,15), "Cashier", "FRONT-END"));
-// employeeArray.add(new Employee("BARACK", "OBAMA", createTimeStamp(13,45), createTimeStamp(18,45), "Cashier", "FRONT-END"));
-// employeeArray.add(new Employee("MICHELLE", "OBAMA", createTimeStamp(7,0), createTimeStamp(13,0), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("DONALD", "WOODS", createTimeStamp(8,30), createTimeStamp(9,30), "Cashier Asst", "FRONT-END")); //WEIRD SEG
+employeeArray.add(new Employee("LIDA", "CARRANZA", createTimeStamp(9,30), createTimeStamp(18,0), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("GUPTA", "SUDESH", createTimeStamp(9,30), createTimeStamp(18,0), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("MOHAMMED", "SARA", createTimeStamp(9,30), createTimeStamp(18,0), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("PETROGRADSKIY", "ARKADIY", createTimeStamp(9,30), createTimeStamp(18,0), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("COLE", "DAVID", createTimeStamp(10,0), createTimeStamp(18,30), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("ANGELITA", "TAPIA", createTimeStamp(10,0), createTimeStamp(18,30), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("GAIL", "GRAYBILL", createTimeStamp(10,15), createTimeStamp(18,45), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("LYDIA", "OPOKU", createTimeStamp(10,15), createTimeStamp(18,45), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("EVELYN", "REYES", createTimeStamp(10,15), createTimeStamp(18,45), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("HIRUTE", "TAKELE", createTimeStamp(10,15), createTimeStamp(18,45), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("ZOILA", "FLORES", createTimeStamp(10,30), createTimeStamp(15,30), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("KENNEDY", "AMPADU", createTimeStamp(10,30), createTimeStamp(12,30), "Cashier", "FRONT-END")); //WEIRD SEG
+employeeArray.add(new Employee("HANIF", "MOHAMMAD", createTimeStamp(10,30), createTimeStamp(15,0), "Cashier", "FRONT-END")); 
+employeeArray.add(new Employee("SAAD", "ANDRAOS", createTimeStamp(10,30), createTimeStamp(19,0), "Cashier Asst", "FRONT-END"));
+employeeArray.add(new Employee("MARGARET", "NTIM", createTimeStamp(10,45), createTimeStamp(15,15), "Cashier", "FRONT-END"));
 
-// employeeArray.add(new Employee("WIZ", "KHALIFA", createTimeStamp(8,30), createTimeStamp(9,30), "Baker", "BAKERY")); //WEIRD SEG
-// employeeArray.add(new Employee("SHECK", "WES", createTimeStamp(9,30), createTimeStamp(18,0), "Baker", "BAKERY"));
-// employeeArray.add(new Employee("JERMAINE", "COLE", createTimeStamp(9,30), createTimeStamp(18,0), "Baker", "BAKERY"));
-// employeeArray.add(new Employee("AUBREY", "GRAHM", createTimeStamp(9,30), createTimeStamp(18,0), "Baker", "BAKERY"));
-// employeeArray.add(new Employee("TYRESSE", "OWENS", createTimeStamp(9,30), createTimeStamp(18,0), "Baker", "BAKERY"));
-// employeeArray.add(new Employee("CANDICE", "OWENS", createTimeStamp(10,0), createTimeStamp(18,30), "Door", "MEMBERSHIP-SERVICE"));
-// employeeArray.add(new Employee("JACK", "BLACK", createTimeStamp(10,0), createTimeStamp(18,30), "Door", "MEMBERSHIP-SERVICE"));
-// employeeArray.add(new Employee("LEONARDO", "DICAPRIO", createTimeStamp(10,15), createTimeStamp(18,45), "Door", "MEMBERSHIP-SERVICE"));
-// employeeArray.add(new Employee("SCARLET", "JOHANSON", createTimeStamp(10,15), createTimeStamp(18,45), "Door", "MEMBERSHIP-SERVICE"));
-// employeeArray.add(new Employee("CHRIS", "HEMSWORTH", createTimeStamp(10,15), createTimeStamp(18,45), "Sandwich Maker", "DELI"));
-// employeeArray.add(new Employee("TOM", "HOLLAND", createTimeStamp(10,15), createTimeStamp(18,45), "Sandwich Maker", "DELI"));
-// employeeArray.add(new Employee("ROBERT", "DOWEY", createTimeStamp(10,30), createTimeStamp(15,30), "Chicken Maker", "DELI"));
-// employeeArray.add(new Employee("CHRIS", "EVANS", createTimeStamp(10,30), createTimeStamp(12,30), "Sandwich Maker", "DELI")); //WEIRD SEG
-// employeeArray.add(new Employee("DARREN", "WATKINS", createTimeStamp(10,30), createTimeStamp(15,0), "Chicken Maker", "DELI")); 
-// employeeArray.add(new Employee("CHRISTOPHER", "NOLAN", createTimeStamp(10,30), createTimeStamp(19,0), "Pharmacist", "PHARMACY"));
-// employeeArray.add(new Employee("ADAM", "LEVINE", createTimeStamp(10,45), createTimeStamp(15,15), "Pharmacist", "PHARMACY"));
-// employeeArray.add(new Employee("BRITTANEY", "SPEARS", createTimeStamp(13,45), createTimeStamp(18,45), "Pharmacist", "PHARMACY"));
-// employeeArray.add(new Employee("MILEY", "CYRUS", createTimeStamp(7,0), createTimeStamp(13,0), "Pharmacist", "PHARMACY"));
+employeeArray.add(new Employee("JIMMY", "HARRIS", createTimeStamp(11,0), createTimeStamp(19,30), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("JOE", "TISCHLER", createTimeStamp(11,0), createTimeStamp(19,30), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("EDISON", "MOLINA", createTimeStamp(11,15), createTimeStamp(19,45), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("GARY", "HOWARD", createTimeStamp(11,30), createTimeStamp(15,30), "Cashier Asst", "FRONT-END"));
+employeeArray.add(new Employee("DAYSI", "HERNANDEZ", createTimeStamp(11,30), createTimeStamp(16,0), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("JONATHAN", "ROMERO", createTimeStamp(11,45), createTimeStamp(17,45), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("CHRISTIANA", "AMIHERE", createTimeStamp(12,0), createTimeStamp(18,30), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("MABEL", "AMOABENG", createTimeStamp(12,45), createTimeStamp(15,15), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("DONALD", "WOODS", createTimeStamp(13,0), createTimeStamp(17,0), "Cashier Asst", "FRONT-END"));
+employeeArray.add(new Employee("REYNA", "PEREIRA", createTimeStamp(13,0), createTimeStamp(18,15), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("KENNEDY", "AMPADU", createTimeStamp(15,0), createTimeStamp(19,0), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("MARIA", "MORALES", createTimeStamp(16,0), createTimeStamp(21,0), "Cashier", "FRONT-END"));//WHAT IF SHE CLOCKED IN EARLY
+employeeArray.add(new Employee("ADEWALE", "ADELEKE", createTimeStamp(16,0), createTimeStamp(21,0), "Cashier Asst", "FRONT-END"));
+employeeArray.add(new Employee("ABI", "MEJIA", createTimeStamp(16,0), createTimeStamp(18,0), "Cashier Asst", "FRONT-END"));
+employeeArray.add(new Employee("MARGARET", "NTIM", createTimeStamp(17,15), createTimeStamp(19,15), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("MABEL", "AMOABENG", createTimeStamp(17,15), createTimeStamp(21,15), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("HANIF", "MOHAMMAD", createTimeStamp(18,0), createTimeStamp(19,0), "Cashier", "FRONT-END"));
+employeeArray.add(new Employee("JUDITH", "AMPONG", createTimeStamp(18,0), createTimeStamp(23,0), "Cashier Asst", "FRONT-END"));
 
-// employeeArray.add(new Employee("JAMES", "ANDERSON", createTimeStamp(8,30), createTimeStamp(9,30), "Baker", "BAKERY")); //WEIRD SEG
-// employeeArray.add(new Employee("MARY", "THOMPSON", createTimeStamp(9,30), createTimeStamp(18,0), "Baker", "BAKERY"));
-// employeeArray.add(new Employee("JOHN", "WILLIAMS", createTimeStamp(9,30), createTimeStamp(18,0), "Baker", "BAKERY"));
-// employeeArray.add(new Employee("PATRICIA", "JOHNSON", createTimeStamp(9,30), createTimeStamp(18,0), "Baker", "BAKERY"));
-// employeeArray.add(new Employee("ROBERT", "BROWN", createTimeStamp(9,30), createTimeStamp(18,0), "Baker", "BAKERY"));
-// employeeArray.add(new Employee("LINDA", "DAVIS", createTimeStamp(10,0), createTimeStamp(18,30), "Door", "MEMBERSHIP-SERVICE"));
-// employeeArray.add(new Employee("MICHAEL", "MILLER", createTimeStamp(10,0), createTimeStamp(18,30), "Door", "MEMBERSHIP-SERVICE"));
-// employeeArray.add(new Employee("ELIZABETH", "WILSON", createTimeStamp(10,15), createTimeStamp(18,45), "Door", "MEMBERSHIP-SERVICE"));
-// employeeArray.add(new Employee("DAVID", "MOORE", createTimeStamp(10,15), createTimeStamp(18,45), "Door", "MEMBERSHIP-SERVICE"));
-// employeeArray.add(new Employee("JENNIFER", "TAYLOR", createTimeStamp(10,15), createTimeStamp(18,45), "Sandwich Maker", "DELI"));
-// employeeArray.add(new Employee("WILLIAM", "CLARK", createTimeStamp(10,15), createTimeStamp(18,45), "Sandwich Maker", "DELI"));
-// employeeArray.add(new Employee("SUSAN", "WHITE", createTimeStamp(10,30), createTimeStamp(15,30), "Chicken Maker", "DELI"));
-// employeeArray.add(new Employee("JOSEPH", "HARRIS", createTimeStamp(10,30), createTimeStamp(12,30), "Sandwich Maker", "DELI")); //WEIRD SEG
-// employeeArray.add(new Employee("JESSICA", "MARTIN", createTimeStamp(10,30), createTimeStamp(15,0), "Chicken Maker", "DELI")); 
-// employeeArray.add(new Employee("THOMAS", "LEWIS", createTimeStamp(10,30), createTimeStamp(19,0), "Pharmacist", "PHARMACY"));
-// employeeArray.add(new Employee("SARAH", "WALKER", createTimeStamp(10,45), createTimeStamp(15,15), "Pharmacist", "PHARMACY"));
-// employeeArray.add(new Employee("CHARLES", "HALL", createTimeStamp(13,45), createTimeStamp(18,45), "Pharmacist", "PHARMACY"));
-employeeArray.add(new Employee("KAREN", "ALLEN", createTimeStamp(7,0), createTimeStamp(13,0), "Pharmacist", "PHARMACY"));
+employeeArray.add(new Employee("SOCHETA", "KHOUN", createTimeStamp(11,0), createTimeStamp(19,30), "Cashier Asst", "FRONT-END"));
+employeeArray.add(new Employee("NILKA", "LAWSON", createTimeStamp(11,0), createTimeStamp(19,30), "Cashier Asst", "FRONT-END"));
+employeeArray.add(new Employee("ABI", "MEJIA", createTimeStamp(11,15), createTimeStamp(19,45), "Cashier Asst", "FRONT-END"));
+employeeArray.add(new Employee("COMFORT", "AFUA", createTimeStamp(11,30), createTimeStamp(15,30), "Cashier Asst", "FRONT-END"));
+employeeArray.add(new Employee("MARGARITA", "ZELAYA", createTimeStamp(11,30), createTimeStamp(16,0), "Cashier Asst", "FRONT-END"));
+employeeArray.add(new Employee("SARA", "ASTUDILLO", createTimeStamp(11,45), createTimeStamp(17,45), "Cashier Asst", "FRONT-END"));
+employeeArray.add(new Employee("SILVIA", "CHEN", createTimeStamp(12,0), createTimeStamp(18,30), "Cashier Asst", "FRONT-END"));
+employeeArray.add(new Employee("DONALD", "WOODS", createTimeStamp(12,45), createTimeStamp(15,15), "Self Check Out(X)", "FRONT-END"));
+employeeArray.add(new Employee("CHRISTIANA", "AMIHERE", createTimeStamp(13,0), createTimeStamp(17,0), "Cashier Asst", "FRONT-END"));
+employeeArray.add(new Employee("JEANNE", "FUMEY", createTimeStamp(13,0), createTimeStamp(18,15), "Self Check Out(X)", "FRONT-END"));
+employeeArray.add(new Employee("KENNEDY", "AMPADU", createTimeStamp(15,0), createTimeStamp(19,0), "Self Check Out(X)", "FRONT-END"));
+employeeArray.add(new Employee("HANIF", "MOHAMMAD", createTimeStamp(16,0), createTimeStamp(21,0), "Self Check Out(X)", "FRONT-END"));//WHAT IF SHE CLOCKED IN EARLY
+employeeArray.add(new Employee("MARGARET", "NTIM", createTimeStamp(16,0), createTimeStamp(21,0), "Self Check Out(X)", "FRONT-END"));
+employeeArray.add(new Employee("MABEL", "AMOBENG", createTimeStamp(16,0), createTimeStamp(18,0), "Self Check Out(X)", "FRONT-END"));
+employeeArray.add(new Employee("DAYSI", "HERNANDEZ", createTimeStamp(17,15), createTimeStamp(19,15), "Self Check Out(X)", "FRONT-END"));
+employeeArray.add(new Employee("COMFORT", "AFUA", createTimeStamp(17,15), createTimeStamp(21,15), "Self Check Out(X)", "FRONT-END"));
+employeeArray.add(new Employee("MARGARITA", "ZELAYA", createTimeStamp(18,0), createTimeStamp(19,0), "Self Check Out(X)", "FRONT-END"));
+employeeArray.add(new Employee("ABI", "MEJIA", createTimeStamp(18,0), createTimeStamp(23,0), "Self Check Out(X)", "FRONT-END"));
+employeeArray.add(new Employee("SARA", "ASTUDILLO", createTimeStamp(18,0), createTimeStamp(20,30), "Self Check Out(X)", "FRONT-END"));
+employeeArray.add(new Employee("ZACH", "LOUKAS", createTimeStamp(9,30), createTimeStamp(18,0), "Cart Crew(X)", "LOT-CREW"));
+employeeArray.add(new Employee("KWAME", "MANU", createTimeStamp(10,0), createTimeStamp(15,0), "Cart Crew(X)", "LOT-CREW"));
+employeeArray.add(new Employee("SHEKU", "CONTEH", createTimeStamp(10,30), createTimeStamp(15,30), "Cart Crew(X)", "LOT-CREW"));
 
-employeeArray.add(new Employee("DANIEL", "YOUNG", createTimeStamp(8,30), createTimeStamp(9,30), "Baker", "BAKERY")); //WEIRD SEG
-employeeArray.add(new Employee("NANCY", "KING", createTimeStamp(12,30), createTimeStamp(21,0), "Baker", "BAKERY"));
+
+
+employeeArray.add(new Employee("ACQUAH", "JOEL", createTimeStamp(9,30), createTimeStamp(18,0), "Cart Crew(X)", "LOT-CREW"));
+employeeArray.add(new Employee("SCHULTZ", "CHAD", createTimeStamp(10,0), createTimeStamp(15,0), "Cart Crew(X)", "LOT-CREW"));
+employeeArray.add(new Employee("ABARIO", "FELIX", createTimeStamp(10,30), createTimeStamp(15,30), "Cart Crew(X)", "LOT-CREW"));
+employeeArray.add(new Employee("JOHNSON", "WILLIE", createTimeStamp(9,30), createTimeStamp(18,0), "Cart Crew(X)", "LOT-CREW"));
+employeeArray.add(new Employee("ROBERTS", "JAMES", createTimeStamp(10,0), createTimeStamp(15,0), "Cart Crew(X)", "LOT-CREW"));
+employeeArray.add(new Employee("PONCE", "JOSE", createTimeStamp(10,30), createTimeStamp(15,30), "Cart Crew(X)", "LOT-CREW"));
+
+employeeArray.add(new Employee("TANAYA", "WILLIAMS", createTimeStamp(12,0), createTimeStamp(18,30), "Sup Hourly", "ADMINISTRATION"));
+employeeArray.add(new Employee("TATIANA", "MEJIA",  createTimeStamp(12,45), createTimeStamp(15,15), "Sup Hourly", "ADMINISTRATION"));
+employeeArray.add(new Employee ("CARMELITA", "DE LEON", createTimeStamp(13,0), createTimeStamp(17,0), "Sup Hourly", "ADMINISTRATION"));
+employeeArray.add(new Employee("FATIMA", "ESTRADA",  createTimeStamp(13,0), createTimeStamp(18,15), "Sup Hourly", "ADMINISTRATION"));
+employeeArray.add(new Employee("BRIAN", "VILLALOBOS",  createTimeStamp(15,0), createTimeStamp(19,0), "Sup Hourly", "ADMINISTRATION"));
+employeeArray.add(new Employee("SOTHEAVY", "NGAN", createTimeStamp(16,0), createTimeStamp(21,0), "Sup Hourly", "ADMINISTRATION"));//WHAT IF SHE CLOCKED IN EARLY
+employeeArray.add(new Employee("WISDOM", "ADOBLE", createTimeStamp(16,0), createTimeStamp(21,0), "Sup Hourly", "ADMINISTRATION"));
+employeeArray.add(new Employee("RODA", "VIERNES", createTimeStamp(16,0), createTimeStamp(18,0), "Sup Hourly", "ADMINISTRATION"));
+employeeArray.add(new Employee("THERESA", "SALBURO", createTimeStamp(17,15), createTimeStamp(19,15), "Mgr Asst Dept", "ADMINISTRATION"));
+employeeArray.add(new Employee("CHRISTOPHER", "RENNIE", createTimeStamp(17,15), createTimeStamp(21,15), "Mgr Asst Dept", "ADMINISTRATION"));
+employeeArray.add(new Employee("IMAN", "BOUSALEH", createTimeStamp(18,0), createTimeStamp(19,0), "Mgr Staff", "ADMINISTRATION"));
+employeeArray.add(new Employee("PONCE", "ROGER", createTimeStamp(18,0), createTimeStamp(23,0), "Sup in Training", "ADMINISTRATION"));
+
+
 
 
 export default employeeArray;
